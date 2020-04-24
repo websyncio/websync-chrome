ef3e9deb44303e5a1e58d603faff8b6542bdb0bb
Aleksey Volkov <wsbaser@gmail.com>
2020-04-24 15:07:26 +0300

    fixed reactor service

--- devtools-panel/app/services/reactor.js
+++ devtools-panel/app/services/reactor.js
@@ -4,24 +4,19 @@ import Service from '@ember/service';
 // taken from http://stackoverflow.com/questions/15308371/custom-events-model-without-using-dom-events-in-javascript
 
 export default Service.extend({
-  constructor(){
-    this.events = {};
-  }
-  
+  events: {},
   registerEvent(eventName){
     this.events[eventName] = new Event(eventName);
-  }
-
+  },
   dispatchEvent(eventName, ...eventArgs){
     this.events[eventName].callbacks.forEach(function(callback){
       callback(...eventArgs);
     });
-  }
-
+  },
   addEventListener(eventName, callback){
     this.events[eventName].registerCallback(callback);
   }
-};
+});
 
 class Event{
   constructor(name){
