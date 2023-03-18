a93635dc44a92775b1349c3d5ba49f714fd5bd92
Aleksey Volkov <aleksey_volkov@epam.com>
2023-03-18 14:11:19 +0100

    update ember

--- devtools-panel/ember-cli-build.js
+++ devtools-panel/ember-cli-build.js
@@ -4,12 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-        'ember-bootstrap': {
-          'importBootstrapTheme': true,
-          'bootstrapVersion': 3,
-          'importBootstrapFont': true,
-          'importBootstrapCSS': true
-        }
     // Add options here
   });
 
